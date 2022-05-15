47280a8692e07535a61dd184fea02b0be34e3f04
vinitvilla <vinithonboard@gmail.com>
2022-05-14 23:11:05 -0400

    added base url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/mca/" : "/",
   css: {
     preprocessorOptions: {
       scss: {
