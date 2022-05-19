1fb20018616bf12cca71246c48473c7d647613ac
vinitvilla <vinithonboard@gmail.com>
2022-05-18 23:45:02 -0400

    Added images

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  base: process.env.NODE_ENV === "production" ? "/mca/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
